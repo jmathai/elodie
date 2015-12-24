8b30107a3efa6d8aa9a65439239c45c6801b27b9
Zingo Andersen <zingo@zingo.org>
2015-12-24 20:39:28 +0100

    Add geolocation cache for distances below 3km
    By reusing lookup's the server need to be contacted a lot less
    Aslo names get a bit clustered and match together a bit better.

--- elodie/constants.py
+++ elodie/constants.py
@@ -3,5 +3,6 @@ from os import path
 debug = True
 application_directory = '{}/.elodie'.format(path.expanduser('~'))
 hash_db = '{}/hash.json'.format(application_directory)
+location_db = '{}/location.json'.format(application_directory)
 script_directory = path.dirname(path.dirname(path.abspath(__file__)))
 exiftool_config = '%s/configs/ExifTool_config' % script_directory
