f1d204cc38b67608d070becb9910248d525dab1e
Jaisen Mathai <jaisen@jmathai.com>
2016-04-12 23:47:41 -0700

    gh-99 Add support for Sony ARW format

--- elodie/media/photo.py
+++ elodie/media/photo.py
@@ -27,7 +27,7 @@ class Photo(Media):
     __name__ = 'Photo'
 
     #: Valid extensions for photo files.
-    extensions = ('dng', 'gif', 'jpeg', 'jpg', 'nef', 'rw2')
+    extensions = ('arw', 'dng', 'gif', 'jpeg', 'jpg', 'nef', 'rw2')
 
     def __init__(self, source=None):
         super(Photo, self).__init__(source)
