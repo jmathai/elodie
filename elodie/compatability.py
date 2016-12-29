82354861351f45bd3c61553072fedf2cd6cedd84
Jaisen Mathai <jaisen@jmathai.com>
2016-12-29 15:14:19 -0800

    gh-167 check for name error

--- elodie/compatability.py
+++ elodie/compatability.py
@@ -1,5 +1,12 @@
 def _decode(string, encoding='utf8'):
-    if not isinstance(string, unicode) and hasattr(string, 'decode'):
+    # Unicode is not defined in Python 3 and throws a NameError
+    # If we encounter a NameError we just assume it's unicode (py3)
+    try:
+        is_unicode = isinstance(string, unicode)
+    except NameError:
+        is_unicode = False
+
+    if is_unicode and hasattr(string, 'decode'):
         return string.decode(encoding)
 
     return string
