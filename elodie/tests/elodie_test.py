c6a1250c0e654a093084b3dae1bd2df3f1bc9e7b
Jaisen Mathai <jaisen@jmathai.com>
2017-01-06 23:31:13 -0800

    Check if string is unicode before encoding

--- elodie/tests/elodie_test.py
+++ elodie/tests/elodie_test.py
@@ -87,12 +87,12 @@ def test_import_file_video():
 
     assert helper.path_tz_fix(os.path.join('2015-01-Jan','California','2015-01-19_12-45-11-video.mov')) in dest_path, dest_path
 
-def test_import_file_path_unicode():
-    raise SkipTest("Temporarily skipping unicode test. sh-167")
+def test_import_file_path_utf8_encoded_ascii():
     temporary_folder, folder = helper.create_working_folder()
     temporary_folder_destination, folder_destination = helper.create_working_folder()
 
     origin = text_type(folder)+u'/unicode'+six_unichr(160)+u'filename.txt'
+    # encode the unicode string to ascii
     origin = origin.encode('utf-8')
 
     shutil.copyfile(helper.get_file('valid.txt'), origin)
@@ -104,7 +104,24 @@ def test_import_file_path_unicode():
     shutil.rmtree(folder)
     shutil.rmtree(folder_destination)
 
-    assert helper.path_tz_fix(os.path.join('2016-04-Apr','Unknown Location',u'2016-04-07_11-15-26-unicode\xa0filename-sample-title.txt')) in dest_path, dest_path
+    assert helper.path_tz_fix(os.path.join('2016-04-Apr','London',u'2016-04-07_11-15-26-unicode\xa0filename-sample-title.txt')) in dest_path, dest_path
+
+def test_import_file_path_unicode():
+    temporary_folder, folder = helper.create_working_folder()
+    temporary_folder_destination, folder_destination = helper.create_working_folder()
+
+    origin = text_type(folder)+u'/unicode'+six_unichr(160)+u'filename.txt'
+
+    shutil.copyfile(helper.get_file('valid.txt'), origin)
+
+    helper.reset_dbs()
+    dest_path = elodie.import_file(origin, folder_destination, False, False, False)
+    helper.restore_dbs()
+
+    shutil.rmtree(folder)
+    shutil.rmtree(folder_destination)
+
+    assert helper.path_tz_fix(os.path.join('2016-04-Apr','London',u'2016-04-07_11-15-26-unicode\xa0filename-sample-title.txt')) in dest_path, dest_path
     
 def test_import_file_allow_duplicate_false():
     temporary_folder, folder = helper.create_working_folder()
