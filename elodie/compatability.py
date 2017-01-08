d539c1208fc7321babc60fbfb28e09100c64ceba
Daniel Draes <Daniel.Draes@servicenow.com>
2017-01-08 22:18:26 +0100

    Moved copyfile to compatability for python3 support

--- elodie/compatability.py
+++ elodie/compatability.py
@@ -1,3 +1,5 @@
+import os
+
 def _decode(string, encoding='utf8'):
     if hasattr(string, 'decode'):
         return string.decode(encoding)
@@ -5,3 +7,25 @@ def _decode(string, encoding='utf8'):
     return string
 
 
+def _copyfile(src, dst):
+    try:
+        O_BINARY = os.O_BINARY
+    except:
+        O_BINARY = 0
+
+    READ_FLAGS = os.O_RDONLY | O_BINARY
+    WRITE_FLAGS = os.O_WRONLY | os.O_CREAT | os.O_TRUNC | O_BINARY
+    BUFFER_SIZE = 128*1024
+        
+    try:
+        fin = os.open(src, READ_FLAGS)
+        stat = os.fstat(fin)
+        fout = os.open(dst, WRITE_FLAGS, stat.st_mode)
+        for x in iter(lambda: os.read(fin, BUFFER_SIZE), ""):
+            os.write(fout, x)
+    finally:
+        try: os.close(fin)
+        except: pass
+        try: os.close(fout)
+        except: pass
+        
