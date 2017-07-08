1802188551ff9199961327c6dfaa79fb4b9469d7
Jaisen Mathai <jaisen@jmathai.com>
2017-07-07 23:35:31 -0700

    Update tests to check for dash instead of \xa0. #228

--- elodie/tests/elodie_test.py
+++ elodie/tests/elodie_test.py
@@ -107,7 +107,7 @@ def test_import_file_path_utf8_encoded_ascii():
     shutil.rmtree(folder)
     shutil.rmtree(folder_destination)
 
-    assert helper.path_tz_fix(os.path.join('2016-04-Apr','London',u'2016-04-07_11-15-26-unicode\xa0filename-sample-title.txt')) in dest_path, dest_path
+    assert helper.path_tz_fix(os.path.join('2016-04-Apr','London',u'2016-04-07_11-15-26-unicode-filename-sample-title.txt')) in dest_path, dest_path
 
 def test_import_file_path_unicode():
     temporary_folder, folder = helper.create_working_folder()
@@ -124,7 +124,7 @@ def test_import_file_path_unicode():
     shutil.rmtree(folder)
     shutil.rmtree(folder_destination)
 
-    assert helper.path_tz_fix(os.path.join('2016-04-Apr','London',u'2016-04-07_11-15-26-unicode\xa0filename-sample-title.txt')) in dest_path, dest_path
+    assert helper.path_tz_fix(os.path.join('2016-04-Apr','London',u'2016-04-07_11-15-26-unicode-filename-sample-title.txt')) in dest_path, dest_path
     
 def test_import_file_allow_duplicate_false():
     temporary_folder, folder = helper.create_working_folder()
