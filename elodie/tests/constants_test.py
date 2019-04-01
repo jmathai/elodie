19be12b0bbea5f3c35824a4b342f23ab6da5d853
Jaisen Mathai <jaisen@jmathai.com>
2019-04-01 00:56:37 -0700

    Make MapQuest parameters configurable #258 (#308)

--- elodie/tests/constants_test.py
+++ elodie/tests/constants_test.py
@@ -26,28 +26,36 @@ def test_debug():
     assert constants.debug == constants.debug, constants.debug
 
 def test_application_directory_default():
-    reload(constants)
     expected_path = '{}/.elodie'.format(os.path.expanduser('~'))
     assert constants.application_directory == expected_path, constants.application_directory
 
 def test_application_directory_override_invalid():
     os.environ['ELODIE_APPLICATION_DIRECTORY'] = '/foo/bar'
     reload(constants)
+    directory_to_check = constants.application_directory
+
+    # reset
+    os.environ['ELODIE_APPLICATION_DIRECTORY'] = ''
+    reload(constants)
+
     expected_path = '{}/.elodie'.format(os.path.expanduser('~'))
-    assert constants.application_directory == expected_path, constants.application_directory
+    assert directory_to_check == expected_path, constants.application_directory
 
 def test_application_directory_override_valid():
     cwd = os.getcwd()
     os.environ['ELODIE_APPLICATION_DIRECTORY'] = cwd
     reload(constants)
+    directory_to_check = constants.application_directory
+    hash_db_to_check = constants.hash_db
 
-    assert constants.application_directory == cwd, constants.application_directory
-    assert cwd in constants.hash_db, constants.hash_db
-
-# must come after test_application_directory_override_valid due to env var reset
-def test_hash_db():
+    # reset
     os.environ['ELODIE_APPLICATION_DIRECTORY'] = ''
     reload(constants)
+
+    assert directory_to_check == cwd, constants.application_directory
+    assert cwd in hash_db_to_check, constants.hash_db
+
+def test_hash_db():
     assert constants.hash_db == '{}/hash.json'.format(constants.application_directory), constants.hash_db
 
 def test_location_db():
@@ -60,6 +68,34 @@ def test_script_directory():
 def test_exiftool_config():
     path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
     assert '{}/configs/ExifTool_config'.format(path) == constants.exiftool_config, constants.exiftool_config
+
+def test_mapquest_base_url_default():
+    assert constants.mapquest_base_url == 'https://open.mapquestapi.com', constants.mapquest_base_url
+
+def test_mapquest_base_url_override():
+    os.environ['ELODIE_MAPQUEST_BASE_URL'] = 'foobar'
+    reload(constants)
+    url_to_check = constants.mapquest_base_url
+
+    # reset
+    os.environ['ELODIE_MAPQUEST_BASE_URL'] = ''
+    reload(constants)
+
+    assert url_to_check == 'foobar', constants.mapquest_base_url
+
+def test_mapquest_Key():
+    assert constants.mapquest_key == None, constants.mapquest_key
+
+def test_mapquest_key_override():
+    os.environ['ELODIE_MAPQUEST_KEY'] = 'foobar'
+    reload(constants)
+    key_to_check = constants.mapquest_key
+
+    # reset
+    os.environ['ELODIE_MAPQUEST_KEY'] = ''
+    reload(constants)
+
+    assert key_to_check == 'foobar', constants.mapquest_key
 
 def test_accepted_language():
     assert constants.accepted_language == 'en', constants.accepted_language
