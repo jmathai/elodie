4e094e4ea53e4b50d9e59b7fada79e20a2861915
Jaisen Mathai <jaisen@jmathai.com>
2019-06-28 00:44:37 -0700

    simplify error handling

--- elodie/plugins/plugins.py
+++ elodie/plugins/plugins.py
@@ -6,6 +6,7 @@ Plugin object.
 from __future__ import print_function
 from builtins import object
 
+from sys import exc_info
 from importlib import import_module
 
 from elodie.config import load_config
@@ -38,11 +39,8 @@ class Plugins(object):
                     self.classes[plugin] = getattr(this_module, plugin)()
                     # We only append to self.plugins if we're able to load the class
                     self.plugins.append(plugin)
-                except ImportError:
-                    log.warn('Could not load plugin {}'.format(plugin))
-                    continue
                 except:
-                    log.warn('Some error occurred initiating plugin {}'.format(plugin))
+                    log.warn('Some error occurred initiating plugin {} - {}'.format(plugin, exc_info()[0]))
                     continue
 
 
