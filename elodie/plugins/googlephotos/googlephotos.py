96166ee85f23759719207b0ca6427bae6acedc40
Jaisen Mathai <jaisen@jmathai.com>
2019-06-28 00:12:43 -0700

    Working plugin module with dummy plugin and tests passing

--- elodie/plugins/googlephotos/googlephotos.py
+++ elodie/plugins/googlephotos/googlephotos.py
@@ -1,3 +1,18 @@
+"""
+Plugin object.
+
+.. moduleauthor:: Jaisen Mathai <jaisen@jmathai.com>
+"""
+from __future__ import print_function
+from builtins import object
+
+
+class GooglePhotos(object):
+    """A class to execute plugin actions."""
+    pass
+
+"""
+
 import json
 
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -16,13 +31,9 @@ try:
     creds = Credentials.from_authorized_user_file(auth_file, scopes)
 except:
     print('no creds')
-    flow = InstalledAppFlow.from_client_secrets_file('/Users/jaisen/Downloads/client_secret_1004259275591-5g51kj0feetbet88o8le5i16hbr3ucb6.apps.googleusercontent.com-3.json', scopes)
-    #creds = tools.run_flow(flow, store)
-    creds = flow.run_local_server(host='localhost',
-                                  port=8080,
-                                  authorization_prompt_message="",
-                                  success_message='The auth flow is complete; you may close this window.',
-                                  open_browser=True)
+    #flow = InstalledAppFlow.from_client_secrets_file('/Users/jaisen/Downloads/client_secret_1004259275591-5g51kj0feetbet88o8le5i16hbr3ucb6.apps.googleusercontent.com-3.json', scopes)
+    flow = InstalledAppFlow.from_client_secrets_file('/Users/jaisen/Downloads/client_secret_1004259275591-ogsk179e96cs0h126qj590mofk86gdqo.apps.googleusercontent.com.json', scopes)
+    creds = flow.run_local_server()
     cred_dict = {
         'token': creds.token,
         'refresh_token': creds.refresh_token,
@@ -39,10 +50,6 @@ except:
 session = AuthorizedSession(creds)
 
 
-"""google_photos = build('photoslibrary', 'v1', http=creds.authorize(Http()))
-results = google_photos.mediaItems().list().execute()
-items = results.get('mediaItems', [])
-print(items)"""
 
 session.headers["Content-type"] = "application/octet-stream"
 session.headers["X-Goog-Upload-Protocol"] = "raw"
@@ -51,8 +58,6 @@ session.headers["X-Goog-Upload-File-Name"] = 'foo.jpg' #os.path.basename(photo_f
 photo_file = open("/Users/jaisen/Downloads/test.png", mode='rb')
 photo_bytes = photo_file.read()
 
-print(photo_bytes)
-
 upload_token = session.post('https://photoslibrary.googleapis.com/v1/uploads', photo_bytes)
 if (upload_token.status_code == 200) and (upload_token.content):
     create_body = json.dumps({"newMediaItems":[{"description":"","simpleMediaItem":{"uploadToken":upload_token.content.decode()}}]}, indent=4)
@@ -60,4 +65,4 @@ if (upload_token.status_code == 200) and (upload_token.content):
     print(resp)
     if "newMediaItemResults" in resp:
         status = resp["newMediaItemResults"][0]["status"]
-        print(status)
+        print(status)"""
