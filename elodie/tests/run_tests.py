6eb6c08e10bea8985be94c9433b11c6362349287
Jaisen Mathai <jaisen@jmathai.com>
2019-07-11 00:59:55 -0700

    add exit code to test runner

--- elodie/tests/run_tests.py
+++ elodie/tests/run_tests.py
@@ -13,3 +13,7 @@ if __name__ == "__main__":
     test_argv = sys.argv
     test_argv.append('--verbosity=2')
     result = nose.run(argv=test_argv)
+    if(result):
+        sys.exit(0)
+    else:
+        sys.exit(1)
