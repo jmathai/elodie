02baf12cb354f50572813f7531f0b61c8170ba82
Jaisen Mathai <jaisen@jmathai.com>
2019-07-11 01:20:00 -0700

    pass environ

--- elodie/tests/run_tests.py
+++ elodie/tests/run_tests.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     test_directory = os.path.dirname(os.path.abspath(__file__))
     test_argv = sys.argv
     test_argv.append('--verbosity=2')
-    result = nose.run(argv=test_argv)
+    result = nose.run(argv=test_argv, env=os.environ)
     if(result):
         sys.exit(0)
     else:
