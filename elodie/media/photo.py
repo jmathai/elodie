87e0420fba15183c9a048daaef727a6998ca6360
Jaisen Mathai <jaisen@jmathai.com>
2019-07-14 17:36:09 -0700

    Make Pillow optional due to external dependencies (#326)
    Fixes #325.
    
    `Pillow` has a number of external dependencies needed for installation. Since most media files are detected correctly with `imghdr` and `Pillow` is used as a fallback we can make it optional.
    
    `Pillow` support was added in #320 to fix #281.

--- elodie/media/photo.py
+++ elodie/media/photo.py
@@ -12,9 +12,7 @@ import os
 import re
 import time
 from datetime import datetime
-from PIL import Image
 from re import compile
-
 
 from elodie import log
 from .media import Media
@@ -37,6 +35,15 @@ class Photo(Media):
 
         # We only want to parse EXIF once so we store it here
         self.exif = None
+
+        # Optionally import Pillow - see gh-325
+        # https://github.com/jmathai/elodie/issues/325
+        self.pillow = None
+        try:
+            from PIL import Image
+            self.pillow = Image
+        except ImportError:
+            pass
 
     def get_date_taken(self):
         """Get the date which the photo was taken.
@@ -98,22 +105,28 @@ class Photo(Media):
         if(extension != 'heic'):
             # gh-4 This checks if the source file is an image.
             # It doesn't validate against the list of supported types.
+            # We check with imghdr and pillow.
             if(imghdr.what(source) is None):
-                # imghdr won't detect all variants of images (https://bugs.python.org/issue28591)
-                # see https://github.com/jmathai/elodie/issues/281
-                # before giving up, we use `pillow` imaging library to detect file type
-                #
-                # It is important to note that the library doesn't decode or load the
-                # raster data unless it really has to. When you open a file,
-                # the file header is read to determine the file format and extract
-                # things like mode, size, and other properties required to decode the file,
-                # but the rest of the file is not processed until later.
-                try:
-                    im = Image.open(source)
-                except IOError:
+                # Pillow is used as a fallback and if it's not available we trust
+                #   what imghdr returned.
+                if(self.pillow is None):
                     return False
+                else:
+                    # imghdr won't detect all variants of images (https://bugs.python.org/issue28591)
+                    # see https://github.com/jmathai/elodie/issues/281
+                    # before giving up, we use `pillow` imaging library to detect file type
+                    #
+                    # It is important to note that the library doesn't decode or load the
+                    # raster data unless it really has to. When you open a file,
+                    # the file header is read to determine the file format and extract
+                    # things like mode, size, and other properties required to decode the file,
+                    # but the rest of the file is not processed until later.
+                    try:
+                        im = self.pillow.open(source)
+                    except IOError:
+                        return False
 
-                if(im.format is None):
-                    return False
-
+                    if(im.format is None):
+                        return False
+        
         return extension in self.extensions
