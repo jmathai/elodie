19dc879fd5f0a22b6f48a2ef489e3133729c1797
Cedric Boucher <onebitalpha@gmail.com>
2023-10-12 21:13:09 -0600

    updated and verified code to change result message there is now a new type of result, duplicate files these are files that already exist in the destination folder and are not imported if not importing duplicates
    I have thoroughly verified that my changes do not cause
    any other part of the code to break or act in an
    unexpected manner. I have also removed some code
    that could never get used:
    elodie/localstorage.py: checksum() could never return None,
    code was not accessible due to earlier return
    because of this:
    elodie/filesystem.py: process_checksum() did not need
    to check if checksum was None after calculating it
    this is what allowed my new code to use None
    as a flag meaning that the file being imported and skipped
    is a duplicate file

--- elodie/result.py
+++ elodie/result.py
@@ -8,17 +8,24 @@ class Result(object):
         self.success = 0
         self.error = 0
         self.error_items = []
+        self.duplicate = 0
+        self.duplicate_items = []
 
     def append(self, row):
         id, status = row
 
-        if status:
+        # status can only be True, False, or None
+        if status is True:
             self.success += 1
+        elif status is None: # status is only ever None if file checksum matched an existing file checksum and is therefore a duplicate file
+            self.duplicate += 1
+            self.duplicate_items.append(id)
         else:
             self.error += 1
             self.error_items.append(id)
 
     def write(self):
+        print("\n")
         if self.error > 0:
             error_headers = ["File"]
             error_result = []
@@ -29,10 +36,21 @@ class Result(object):
             print(tabulate(error_result, headers=error_headers))
             print("\n")
 
+        if self.duplicate > 0:
+            duplicate_headers = ["File"]
+            duplicate_result = []
+            for id in self.duplicate_items:
+                duplicate_result.append([id])
+
+            print("****** DUPLICATE (NOT IMPORTED) DETAILS ******")
+            print(tabulate(duplicate_result, headers=duplicate_headers))
+            print("\n")
+
         headers = ["Metric", "Count"]
         result = [
                     ["Success", self.success],
                     ["Error", self.error],
+                    ["Duplicate, not imported", self.duplicate]
                  ]
 
         print("****** SUMMARY ******")
